68059706314065a8656c8fbbff6186ae07ccd993
Lucas Baggio <lucas.baggio.silva@gmail.com>
2025-07-12 18:52:29 -0300

    Feat - Criação da entidade Order e sua enum de status

---
+++ Core/Entities/Order.cs
@@ -0,0 +1,15 @@
+using Core.Enums;
+
+namespace Core.Entities;
+
+public class Order : BaseEntity
+{
+    public decimal Total { get; set; }
+    public decimal Freight { get; set; }
+    public decimal Discount { get; set; }
+    public OrderStatus Status { get; set; }
+    public int CustomerId { get; set; }
+    public Customer? Customer { get; set; }
+
+
+}
---
+++ Core/Enums/OrderStatus.cs
@@ -0,0 +1,9 @@
+namespace Core.Enums;
+
+public enum OrderStatus
+{
+    Pending,
+    Paid,
+    Shipped,
+    Cancelled
+}
